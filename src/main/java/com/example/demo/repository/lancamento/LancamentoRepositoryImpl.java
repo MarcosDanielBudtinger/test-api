f200f6924ca3a0bd74691d243bc067cc0ea60f02
Marcos Daniel <marcos.mdbbr@gmail.com>
2021-07-13 15:19:06 -0300

    criado metamodel para lancamento, criada consulta por queryParams para lancamento com descricao like, dataVencimentoDe e dataVencimentoAte, também incluida paginação na consulta de lançamento utilizando size e page

--- src/main/java/com/example/demo/repository/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/lancamento/LancamentoRepositoryImpl.java
@@ -1,12 +1,23 @@
 package com.example.demo.repository.lancamento;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import com.example.demo.model.Lancamento;
+import com.example.demo.model.Lancamento_;
 import com.example.demo.repository.filter.LancamentoFilter;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
@@ -14,8 +25,64 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
     EntityManager manager;
 
     @Override
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
-        return null;
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
+        Root<Lancamento> root = criteria.from(Lancamento.class);
+
+        // Restrições
+        Predicate[] predicates = inserirRestricoes(lancamentoFilter, builder, root);
+        criteria.where(predicates);
+
+        TypedQuery<Lancamento> query = manager.createQuery(criteria);
+
+        inserirRestricoesDePaginacao(query, pageable);
+
+        return new PageImpl<>(query.getResultList(), pageable, query.getResultList().size());
+    }
+
+    private void inserirRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+        int paginaAtual = pageable.getPageNumber();
+        int totalRegistrosPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalRegistrosPorPagina);
+
+    }
+    // Desnecessário sendo que temos o total de registros do filtro dando um .size
+    // na lista.
+    /*
+     * private Long total(LancamentoFilter lancamentoFilter) { CriteriaBuilder
+     * builder = manager.getCriteriaBuilder(); CriteriaQuery<Long> criteria =
+     * builder.createQuery(Long.class); Root<Lancamento> root =
+     * criteria.from(Lancamento.class);
+     * 
+     * Predicate[] predicates = inserirRestricoes(lancamentoFilter, builder, root);
+     * criteria.where(predicates);
+     * 
+     * criteria.select(builder.count(root)); return
+     * manager.createQuery(criteria).getSingleResult(); }
+     */
+
+    private Predicate[] inserirRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
+            Root<Lancamento> root) {
+        List<Predicate> predicates = new ArrayList<>();
+        if (lancamentoFilter.getDescricao() != null) {
+            predicates.add(builder.like(builder.lower(root.get(Lancamento_.descricao)),
+                    "%" + lancamentoFilter.getDescricao().toLowerCase() + "%"));
+        }
+
+        if (lancamentoFilter.getDataVencimentoDe() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+                    lancamentoFilter.getDataVencimentoDe()));
+        }
+
+        if (lancamentoFilter.getDataVencimentoAte() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento),
+                    lancamentoFilter.getDataVencimentoAte()));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 
 }
--- src/main/java/com/example/demo/resource/PessoaResource.java
+++ src/main/java/com/example/demo/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.example.demo.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +10,8 @@ import com.example.demo.model.Pessoa;
 import com.example.demo.repository.PessoaRepository;
 import com.example.demo.service.PessoaService;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +23,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("pessoas")
@@ -46,21 +42,20 @@ public class PessoaResource {
         List<Pessoa> response = pessoaRepository.findAll();
         return ResponseEntity.ok(response);
     }
-   
+
     @PostMapping
-    public ResponseEntity<Pessoa> criar (@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
         publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoa.getId()));
         return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> buscarPeloId(@PathVariable Long id){
+    public ResponseEntity<?> buscarPeloId(@PathVariable Long id) {
         Optional<Pessoa> pessoa = pessoaRepository.findById(id);
-        return pessoa.isPresent() ? 
-            ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+        return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
     }
-    
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long codigo) {
@@ -68,14 +63,14 @@ public class PessoaResource {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa ){
+    public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa) {
         Pessoa pessoaSalva = pessoaService.atualizar(id, pessoa);
         return ResponseEntity.ok(pessoaSalva);
     }
 
     @PutMapping("/{id}/ativo")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizarPropriedadeAtivo(@PathVariable Long id, @RequestBody Boolean ativo){
+    public void atualizarPropriedadeAtivo(@PathVariable Long id, @RequestBody Boolean ativo) {
         pessoaService.atualizarPropriedadeAtivo(id, ativo);
     }
 }
